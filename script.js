44ccddc357c391ad030ce519307abf8cd190dd0f
tonyr1977 <56265104+tonyr1977@users.noreply.github.com>
2019-10-08 11:00:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Tony Ren"
+let userName = "tonyr1977";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
